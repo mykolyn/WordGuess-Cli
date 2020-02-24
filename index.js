77ec8604f40d60ef63fed0bb2b50105737743fbe
mykolyn <mykolyn@gmail.com>
2020-02-23 17:03:11 -0800

    init commit

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const inquirer = require("inquirer");
 
 function run(){
+    inquirer.prompt([
 
+        {
+          type: "input",
+          name: "userInput",
+          message: "Guess a letter"
+        }
+      
+      // After the prompt, store the user's response in a variable called location.
+      ]).then(function(res) {
+          console.log(res.userInput)
 }
 
-run()
+      )}
+      run()
